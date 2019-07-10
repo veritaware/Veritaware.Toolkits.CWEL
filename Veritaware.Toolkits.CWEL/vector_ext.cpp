4d95553c1fcb4994fc1ae0e01c0cd5ad42ceb23f
Nidrax <dpartcompany@gmail.com>
2019-07-10 14:58:54 +0200

    add vector extensions

---
+++ Veritaware.Toolkits.CWEL/vector_ext.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "vector_ext.h"
+
+template<typename T>
+void veritaware::AddRange(std::vector<T> &dst, std::vector<T> &src)
+{
+	dst.insert(dst.end(), src.begin(), src.end());
+}
+
+template<typename T>
+void veritaware::Reverse(std::vector<T> &v)
+{
+	std::reverse(v.begin(), v.end());
+}
---
+++ Veritaware.Toolkits.CWEL/vector_ext.h
@@ -0,0 +1,15 @@
+#ifndef VERITAWARE_TOOLKITS_CWEL_VECTOR_EXT_
+#define VERITAWARE_TOOLKITS_CWEL_VECTOR_EXT_
+
+#include <vector>
+
+namespace veritaware
+{
+	template<typename T>
+	void AddRange(std::vector<T> &dst, std::vector<T> &src);
+
+	template<typename T>
+	void Reverse(std::vector<T> &v);
+}
+
+#endif //VERITAWARE_TOOLKITS_CWEL_VECTOR_EXT_
