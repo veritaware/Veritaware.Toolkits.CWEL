0b673606405a0b572b721005c12256c73fc3151f
Daniel Praźmo <d.prazmo@icloud.com>
2024-05-29 02:05:55 +0200

    Implement value types wrapper classes Remove generic GetHashCode() implementation in favor of value type wrappers

--- include/IGetHashCode.hpp
+++ include/IGetHashCode.hpp
@@ -4,18 +4,7 @@
 
 namespace vwr
 {
-	int32_t GetHashCode(bool value);
-	int32_t GetHashCode(int8_t value);
-	int32_t GetHashCode(uint8_t value);
-	int32_t GetHashCode(int16_t value);
-	int32_t GetHashCode(uint16_t value);
-	int32_t GetHashCode(int32_t value);
-	int32_t GetHashCode(uint32_t value);
-	int32_t GetHashCode(int64_t value);
-	int32_t GetHashCode(uint64_t value);
     int32_t GetHashCode(size_t value);
-	int32_t GetHashCode(float value);
-	int32_t GetHashCode(double value);
 	int32_t GetHashCode(const char * value);
 	int32_t GetHashCode(const std::string & value);
 	int32_t CombineHashCodes(int32_t hash1, int32_t hash2);
