576022b65f2366ada9ec0fd250b423d5b64710fc
Daniel Praźmo <d.prazmo@icloud.com>
2024-05-29 02:33:55 +0200

    Add value type objects aliases header

---
+++ include/TypeAliases.hpp
@@ -0,0 +1,37 @@
+#ifndef VWRTK_CWEL_TYPE_ALIASES_HPP
+#define VWRTK_CWEL_TYPE_ALIASES_HPP
+
+#include "Object.hpp"
+
+using object   = vwr::Object;
+using bool_o   = vwr::Boolean;
+using int8_o   = vwr::SByte;
+using i8_o     = vwr::SByte;
+using sbyte_o  = vwr::SByte;
+using uint8_o  = vwr::Byte;
+using u8_o     = vwr::Byte;
+using byte_o   = vwr::Byte;
+using int16_o  = vwr::Int16;
+using i16_o    = vwr::Int16;
+using short_o  = vwr::Int16;
+using uint16_o = vwr::UInt16;
+using u16_o    = vwr::UInt16;
+using ushort_o = vwr::UInt16;
+using int32_o  = vwr::Int32;
+using i32_o    = vwr::Int32;
+using int_o    = vwr::Int32;
+using uint32_o = vwr::UInt32;
+using u32_o    = vwr::UInt32;
+using uint_o   = vwr::UInt32;
+using int64_o  = vwr::Int64;
+using i64_o    = vwr::Int64;
+using long_o   = vwr::Int64;
+using uint64_o = vwr::UInt64;
+using u64_o    = vwr::UInt64;
+using ulong_o  = vwr::UInt64;
+using float_o  = vwr::Single;
+using f32_o    = vwr::Single;
+using double_o = vwr::Double;
+using f64_o    = vwr::Double;
+
+#endif //VWRTK_CWEL_TYPE_ALIASES_HPP
