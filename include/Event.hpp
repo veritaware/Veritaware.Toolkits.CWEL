094518716aa19343403729ef37760dc77f8969dd
Daniel Praźmo <d.prazmo@icloud.com>
2024-06-22 21:41:13 +0200

    Summarize Veritaware classes.

--- include/Event.hpp
+++ include/Event.hpp
@@ -6,6 +6,10 @@
 
 namespace vwr
 {
+    /// <summary>
+    /// Provides the base class for classes that contain event data,
+    /// and a value to use for events without event data.
+    /// </summary>
     class EventArgs : public Object
     {
     public:
@@ -16,14 +20,30 @@ namespace vwr
         EventArgs() = default;
     };
 
+    /// <summary>
+    /// Provides a class to notify other classes whenever something of interest occurs.
+    /// Calls back a delegate method when the event is raised.
+    /// </summary>
     template<std::derived_from<EventArgs> TEventArgs>
     class Event : public Object
     {
+        /// <summary>
+        /// Represents the method that handles an event.
+        /// </summary>
         using delegate = void(*)(Object*, TEventArgs*);
     public:
         explicit Event(const Object& parent);
+        /// <summary>
+        /// Subscribes to the event invocation.
+        /// </summary>
         void operator+=(delegate handler);
+        /// <summary>
+        /// Unsubscribes from the event invocation.
+        /// </summary>
         void operator-=(delegate handler);
+        /// <summary>
+        /// Invokes event callbacks. Can be performed by the event's parent object only.
+        /// </summary>
         void Invoke(Object* sender, TEventArgs* e);
 
     protected:
--- include/Extensions/Vector.hpp
+++ include/Extensions/Vector.hpp
@@ -5,12 +5,21 @@
 
 namespace vwr
 {
+    /// <summary>
+    /// Adds the elements of the specified collection to the end of the Vector.
+    /// </summary>
     template<typename T>
     void AddRange(std::vector<T> &dst, const std::vector<T> &src);
 
+    /// <summary>
+    /// Reverses the order of all the elements in the Vector.
+    /// </summary>
     template<typename T>
     void Reverse(std::vector<T> &v);
 
+    /// <summary>
+    /// Removes the first occurrence of a specific object from the Vector.
+    /// </summary>
     template<typename T>
     void Remove(std::vector<T> &v, const T &value);
 }
