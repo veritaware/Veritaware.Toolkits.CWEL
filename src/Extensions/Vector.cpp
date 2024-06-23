3bc221afe33fbcee3233014621b2aaec42619375
Daniel Praźmo <d.prazmo@icloud.com>
2024-06-23 09:01:16 +0200

    Extend std::vector with a wrapper class

--- src/Extensions/Vector.cpp
+++ src/Extensions/Vector.cpp
@@ -5,19 +5,37 @@
 namespace vwr
 {
     template<typename T>
-    void AddRange(std::vector<T> &dst, const std::vector<T> &src)
+    void vwr::vector<T>::AddRange(const std::vector<T> &src)
+    {
+        AddRange(*this, src);
+    }
+
+    template<typename T>
+    void vwr::vector<T>::AddRange(std::vector<T> &dst, const std::vector<T> &src)
     {
         dst.insert(dst.end(), src.begin(), src.end());
     }
 
     template<typename T>
-    void Reverse(std::vector<T> &v)
+    void vwr::vector<T>::Reverse()
+    {
+        Reverse(*this);
+    }
+
+    template<typename T>
+    void vwr::vector<T>::Reverse(std::vector<T> &v)
     {
         std::reverse(v.begin(), v.end());
     }
 
     template<typename T>
-    void Remove(std::vector<T> &v, const T &value)
+    void vwr::vector<T>::Remove(const T &value)
+    {
+        Remove(*this, value);
+    }
+
+    template<typename T>
+    void vwr::vector<T>::Remove(std::vector<T> &v, const T &value)
     {
         v.erase(std::remove(v.begin(), v.end(), value), v.end());
     }
