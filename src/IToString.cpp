b868706c83562b033ce7fa31455122523646e6d3
Daniel Praźmo <d.prazmo@icloud.com>
2026-03-26 07:57:17 +0100

    Remove old v0.1 contents

--- src/IToString.cpp
+++
@@ -1,9 +0,0 @@
-#include "IToString.hpp"
-
-namespace vwr {
-    std::ostream& operator<< (std::ostream& stream, const IToString& iToString)
-    {
-        stream << iToString.ToString();
-        return stream;
-    }
-}
